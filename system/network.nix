ea2c928123fd8fc08432a897eec47cd5dff6dfdd
chatondearu <823314+chatondearu@users.noreply.github.com>
2026-03-27 22:06:41 +0100

    ✨ Update NixOS audio and network configurations
    - Enabled PipeWire with additional settings for AirPlay/RAOP support and local Snapcast client.
    - Updated firewall rules to allow specific TCP and UDP ports for Snapcast and mDNS services.
    - Enhanced Avahi service configuration for improved network service discovery.

--- system/network.nix
+++ system/network.nix
@@ -3,10 +3,24 @@
 {
   networking.hostName = "neo-nix";
 
-  # Enable networking
   networking.networkmanager.enable = true;
+  networking.firewall.allowedTCPPorts = [
+    2242  # SSH
+    8009  # TCP SYN
+    9000  # UPnP control ports (dynamic, opened via conntrack normally)
+    # 1704  # Snapcast server
+    # 1780  # Snapcast server
+    1705  # Snapcast client
+  ];
+  networking.firewall.allowedUDPPorts = [
+    8009  # UDP SYN
+    5353  # mDNS
+  ];
 
-  # SSH configuration with security hardening
+  networking.firewall.allowedUDPPortRanges = [
+    { from = 32768; to = 61000; }  # SSDP discovery (sblast)
+  ];
+
   services.openssh = {
     enable = true;
     ports = [ 2242 ];
@@ -19,9 +33,27 @@
     openFirewall = true;
   };
 
-  # Network Service Discovery for mDNS (avahi-daemon). Seems to be needed by mDNS.
+  # mDNS + UPnP/DLNA discovery
   services.avahi = {
     enable = true;
     nssmdns4 = true;
+    openFirewall = true; # ouvre automatiquement 5353/udp
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+      userServices = true;
+    };
+
+    # extraServiceFiles.snapserver = ''
+    #   <?xml version="1.0" standalone='no'?><!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+    #   <service-group>
+    #     <name replace-wildcards="yes">NixOS Snapcast (%h)</name>
+    #     <service>
+    #       <type>_snapcast._tcp</type>
+    #       <port>1704</port>
+    #     </service>
+    #   </service-group>
+    # '';
   };
 }
